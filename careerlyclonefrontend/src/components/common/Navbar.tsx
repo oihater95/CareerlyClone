67dbe0b66bebc624a2251a013d99989584183865
oihater95 <oihater95@gmail.com>
2024-09-07 16:45:14 +0900

    [bugfix] NavBar Main 버튼 임시 처리
    NavBar Main 버튼 누를 시 메인페이지로 이동하는 로직 중
    로그인 토큰 직접 삽입하여 이동(임시처리)

--- careerlyclonefrontend/src/components/common/Navbar.tsx
+++ careerlyclonefrontend/src/components/common/Navbar.tsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 import '../../css/pages/NavBar.scss';
 
 const NavBar = () => {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
   let loginOrLogoutText = 'Login';
   if (isLogin) loginOrLogoutText = 'Logout';
+  // 카카오 서버로 부터 받은 accessToken 임시처리
+  const params = new URL(document.URL).searchParams;
+  // 인가코드는 백엔드로 넘겨서 reponse로 jwt 토큰 받기
+  const code = params.get('code');
   return (
     <div className='navbar-root'>
       <div className='navbar-left'>
         {/* <span>Main</span> */}
-        <Link className='navbar-btn' to={isLogin ? '/oauth/kakao/callback' : '/login'}>Main</Link>
+        <Link className='navbar-btn' to={isLogin ? `/oauth/kakao/callback?code=${code}` : '/login'}>Main</Link>
       </div>
       <div className='navbar-right'>
         {/* <span>{loginOrLogoutText}</span> */}
