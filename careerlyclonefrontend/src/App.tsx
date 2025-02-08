1994ef7ffc6cc850f7ea9c65426ae037a7049fc3
oihater95 <oihater95@gmail.com>
2025-02-08 17:14:11 +0900

    [bugfix] prettier 적용
    prettier 적용

--- careerlyclonefrontend/src/App.tsx
+++ careerlyclonefrontend/src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingLoginPage from './pages/LandingLoginPage';
-import MainPage from './pages/MainPage';
-import NavBar from './components/common/Navbar';
-import PostingDetailComponent from './components/posting/PostingDetailComponent';
-import './App.scss'
-import CreateFormComponent from './components/posting/CreateFormComponent';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingLoginPage from "./pages/LandingLoginPage";
+import MainPage from "./pages/MainPage";
+import NavBar from "./components/common/Navbar";
+import PostingDetailComponent from "./components/posting/PostingDetailComponent";
+import "./App.scss";
+import CreateFormComponent from "./components/posting/CreateFormComponent";
 
 const App: React.FC = () => {
   return (
     <Router>
+      <NavBar />
       <div className="root-container">
-        <NavBar />
         <Routes>
           <Route path="/" element={<LandingLoginPage />} />
           <Route path="/oauth/kakao/callback" element={<MainPage />} />
           <Route path="/posting/:id" element={<PostingDetailComponent />} />
-          <Route path='createPost' element={<CreateFormComponent />} />
+          <Route path="createPost" element={<CreateFormComponent />} />
         </Routes>
       </div>
     </Router>
